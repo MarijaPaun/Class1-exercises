9521f69f6160b6e02f374f029f579ad86e3b3c90
MarijaPaun <95476002+MarijaPaun@users.noreply.github.com>
2021-12-22 16:18:17 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+console.log("Conected")
+
+console.log("oneFeetInMeters")
+let oneFeetInMeters = 0.3048
+console.log(oneFeetInMeters)
+
+console.log("numberOfFeet")
+let numberOfFeet = 100
+console.log(numberOfFeet)
+
+console.log("feetToMeters")
+let feetToMeters = oneFeetInMeters * numberOfFeet
+console.log(feetToMeters)
