2e6ac05c04746445fc0151c75d2d5f8667de5af4
MarijaPaun <95476002+MarijaPaun@users.noreply.github.com>
2021-12-22 16:19:06 +0100

    Add files via upload

---
+++ exerciseNo2.js
@@ -0,0 +1,13 @@
+console.log("connected")
+
+console.log("sideA")
+let sideA = 20
+console.log(sideA)
+
+console.log("sideB")
+let sideB = 40
+console.log(sideB)
+
+console.log("area")
+let area = sideA * sideB
+console.log(area)
